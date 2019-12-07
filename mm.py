88e67a3654a901e4e991a46096aae28e6df6a93e
deepika1708 <54228120+deepika1708@users.noreply.github.com>
2019-12-07 22:17:50 +0530

    Create mm.py

---
+++ mm.py
@@ -0,0 +1,52 @@
+print("\t\tFIND MONTH")
+  from datetime import datetime as d
+x = int(input("Enter month number : "))
+if x == 1:
+    print("Jenury\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x == 2:
+    print("Febrary\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x == 3 :
+    print("March \n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==4 :
+    print("April\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==5 :
+    print("Mai\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==6 :
+    print("June\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==7:
+    print("July\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==8 :
+    print("August\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==9 :
+    print("September\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==10 :
+    print("October\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==11 :
+    print("Nubember\n")
+    x = d.now()
+    print("Current date and time is :",x)
+elif x==12 :
+    print("December\n")
+    x = d.now()
+    print("\nCurrent date and time is :",x)
+else:print("Wrong choice ")
